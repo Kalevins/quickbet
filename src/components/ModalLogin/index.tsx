c9eedba2a63c0764be17d8ff7ca1cac95865e0a7
Kalevins <kevinmunoz01@hotmail.com>
2024-09-10 09:34:41 -0500

    [Fix] login modal styles

--- src/components/ModalLogin/index.tsx
+++ src/components/ModalLogin/index.tsx
@@ -107,185 +107,175 @@ export const ModalLogin: FC = (): JSX.Element => {
   };
 
   return (
-    <>
-      {isOpen && (
-        <div
-          className={styles.containerModal}
-          onClick={() => handleOpenModal(false)}
-        >
-          <div
-            className={styles.modal}
-            onClick={(event) => event.stopPropagation()}
-          >
-            <div className={styles.formContainer}>
-              {activeOption !== activeOptions.logout && (
-                <div className={styles.buttonsContainer}>
-                  <button
-                    className={styles.button}
-                    id={
-                      activeOption === activeOptions.signUp ? styles.active : ""
-                    }
-                    onClick={() => {
-                      setError("");
-                      setSignUpOption("");
-                      setActiveOption(activeOptions.signUp);
-                    }}
-                  >
-                    Sign Up
-                  </button>
-                  <button
-                    className={styles.button}
-                    id={
-                      activeOption === activeOptions.login ? styles.active : ""
-                    }
-                    onClick={() => {
-                      setError("");
-                      setActiveOption(activeOptions.login);
-                    }}
-                  >
-                    Login
-                  </button>
-                </div>
-              )}
-
-              {activeOption === activeOptions.signUp && (
-                <div className={styles.form}>
-                  {signUpOption === "" ? (
-                    <>
-                      {signUpOptions.map((option) => (
-                        <button
-                          key={option.id}
-                          className={styles.buttonForm}
-                          onClick={() => setSignUpOption(option.id)}
-                        >
-                          {option.placeholder}
-                        </button>
-                      ))}
-                    </>
-                  ) : (
-                    <form
-                      className={styles.formClass}
-                      onSubmit={handleRegister}
-                    >
-                      <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        className={styles.input}
-                      />
-                      <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        className={styles.input}
-                      />
-                      <input
-                        type="password"
-                        name="confirmPassword"
-                        placeholder="Confirm Password"
-                        className={styles.input}
-                      />
-                      <button className={styles.buttonForm}>Continue</button>
-                    </form>
-                  )}
-                </div>
-              )}
-
-              {activeOption === activeOptions.login && (
-                <div className={styles.form}>
-                  <form className={styles.formClass} onSubmit={handleLogin}>
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      className={styles.input}
-                    />
-                    <input
-                      type="password"
-                      name="password"
-                      placeholder="Password"
-                      className={styles.input}
-                    />
-                    <button className={styles.buttonForm}>Continue</button>
-                  </form>
-                </div>
-              )}
-
-              {activeOption === activeOptions.logout && (
-                <div className={styles.form}>
-                  <button className={styles.buttonForm} onClick={handleLogout}>
-                    Logout
-                  </button>
-                </div>
-              )}
-
-              <div className={styles.messages}>
-                <small className={styles.error}>{error}</small>
-                <small className={styles.info}>
-                  For any questions, reach out to support@Quickbetdmovies.com
-                </small>
-              </div>
+    <div
+      className={styles.containerModal}
+      id={isOpen ? "" : styles.inactive}
+      onClick={() => handleOpenModal(false)}
+    >
+      <div
+        className={styles.modal}
+        onClick={(event) => event.stopPropagation()}
+      >
+        <div className={styles.formContainer}>
+          {activeOption !== activeOptions.logout && (
+            <div className={styles.buttonsContainer}>
+              <button
+                className={styles.button}
+                id={activeOption === activeOptions.signUp ? styles.active : ""}
+                onClick={() => {
+                  setError("");
+                  setSignUpOption("");
+                  setActiveOption(activeOptions.signUp);
+                }}
+              >
+                Sign Up
+              </button>
+              <button
+                className={styles.button}
+                id={activeOption === activeOptions.login ? styles.active : ""}
+                onClick={() => {
+                  setError("");
+                  setActiveOption(activeOptions.login);
+                }}
+              >
+                Login
+              </button>
             </div>
+          )}
 
-            {activeOption === activeOptions.signUp && (
-              <div className={styles.infoContainer}>
-                <h1>Welcome to Quickbet Movies!</h1>
-                <p>
-                  🎬 Ready to unlock a universe of cinematic delights? Sign up
-                  now and start your journey with us!
-                </p>
-                <div className={styles.imageContainer}>
-                  <Image
-                    className={styles.image}
-                    src={ImageSignUp}
-                    alt="Sign Up"
-                    width={300}
-                    height={300}
+          {activeOption === activeOptions.signUp && (
+            <div className={styles.form}>
+              {signUpOption === "" ? (
+                <>
+                  {signUpOptions.map((option) => (
+                    <button
+                      key={option.id}
+                      className={styles.buttonForm}
+                      onClick={() => setSignUpOption(option.id)}
+                    >
+                      {option.placeholder}
+                    </button>
+                  ))}
+                </>
+              ) : (
+                <form className={styles.formClass} onSubmit={handleRegister}>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    className={styles.input}
                   />
-                </div>
-              </div>
-            )}
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className={styles.input}
+                  />
+                  <input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirm Password"
+                    className={styles.input}
+                  />
+                  <button className={styles.buttonForm}>Continue</button>
+                </form>
+              )}
+            </div>
+          )}
 
-            {activeOption === activeOptions.login && (
-              <div className={styles.infoContainer}>
-                <h1>Welcome back to Quickbet Movies!</h1>
-                <p>
-                  🍿 Ready to dive into the world of unlimited entertainment?
-                  Enter your credentials and let the cinematic adventure begin!
-                </p>
-                <div className={styles.imageContainer}>
-                  <Image
-                    className={styles.image}
-                    src={ImageSignIn}
-                    alt="Login"
-                    width={300}
-                    height={300}
-                  />
-                </div>
-              </div>
-            )}
+          {activeOption === activeOptions.login && (
+            <div className={styles.form}>
+              <form className={styles.formClass} onSubmit={handleLogin}>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  className={styles.input}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className={styles.input}
+                />
+                <button className={styles.buttonForm}>Continue</button>
+              </form>
+            </div>
+          )}
 
-            {activeOption === activeOptions.logout && (
-              <div className={styles.infoContainer}>
-                <h1>Goodbye!</h1>
-                <p>
-                  🎥 Thank you for choosing Quickbet Movies! We hope you had a
-                  great time with us. See you soon!
-                </p>
-                <div className={styles.imageContainer}>
-                  <Image
-                    className={styles.image}
-                    src={ImageSignIn}
-                    alt="Login"
-                    width={300}
-                    height={300}
-                  />
-                </div>
-              </div>
-            )}
+          {activeOption === activeOptions.logout && (
+            <div className={styles.form}>
+              <button className={styles.buttonForm} onClick={handleLogout}>
+                Logout
+              </button>
+            </div>
+          )}
+
+          <div className={styles.messages}>
+            <small className={styles.error}>{error}</small>
+            <small className={styles.info}>
+              For any questions, reach out to support@Quickbetdmovies.com
+            </small>
           </div>
         </div>
-      )}
-    </>
+
+        {activeOption === activeOptions.signUp && (
+          <div className={styles.infoContainer}>
+            <h1>Welcome to Quickbet Movies!</h1>
+            <p>
+              🎬 Ready to unlock a universe of cinematic delights? Sign up now
+              and start your journey with us!
+            </p>
+            <div className={styles.imageContainer}>
+              <Image
+                className={styles.image}
+                src={ImageSignUp}
+                alt="Sign Up"
+                width={300}
+                height={300}
+              />
+            </div>
+          </div>
+        )}
+
+        {activeOption === activeOptions.login && (
+          <div className={styles.infoContainer}>
+            <h1>Welcome back to Quickbet Movies!</h1>
+            <p>
+              🍿 Ready to dive into the world of unlimited entertainment? Enter
+              your credentials and let the cinematic adventure begin!
+            </p>
+            <div className={styles.imageContainer}>
+              <Image
+                className={styles.image}
+                src={ImageSignIn}
+                alt="Login"
+                width={300}
+                height={300}
+              />
+            </div>
+          </div>
+        )}
+
+        {activeOption === activeOptions.logout && (
+          <div className={styles.infoContainer}>
+            <h1>Goodbye!</h1>
+            <p>
+              🎥 Thank you for choosing Quickbet Movies! We hope you had a great
+              time with us. See you soon!
+            </p>
+            <div className={styles.imageContainer}>
+              <Image
+                className={styles.image}
+                src={ImageSignIn}
+                alt="Login"
+                width={300}
+                height={300}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
